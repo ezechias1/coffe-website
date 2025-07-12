64f384052bada098ce41c7740342af8d53845cd5
ezechias <ezechiasmulamba@gmail.com>
2025-07-13 01:09:39 +0200

    added the testimonials section

--- scripts.js
+++ scripts.js
@@ -7,3 +7,18 @@ menuOpenButton.addEventListener("click", () => {
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+//Intialize swiper
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
